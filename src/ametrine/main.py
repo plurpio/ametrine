b543114bfc18ddd586c93e6572c9e105ef68975d
Plurpio <148961679+plurpio@users.noreply.github.com>
2024-03-09 18:31:50 +1100

    debug: made DEBUG env var enable logging

--- src/ametrine/main.py
+++ src/ametrine/main.py
@@ -4,6 +4,7 @@
 
 import sys
 import logging
+import os
 
 #
 # Configure pretty logging
@@ -15,10 +16,12 @@ logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%H:
 logging.basicConfig(format='%(asctime)s %(levelname)s %(module)s -> %(funcName)s: %(message)s', datefmt='%H:%M:%S', level=logging.ERROR)
 logging.basicConfig(format='%(asctime)s %(levelname)s %(module)s -> %(funcName)s: %(message)s', datefmt='%H:%M:%S', level=logging.CRITICAL)
 
-logging.disable(logging.CRITICAL)
-if len(sys.argv) >= 2 and sys.argv[1] == "debug":
-    logging.disable(logging.NOTSET)
-    logging.info("Logging enabled.")
+if os.getenv("DEBUG") or len(sys.argv) >= 2 and sys.argv[1] == "debug":
+    logging.getLogger().setLevel(logging.DEBUG)
+    logging.debug("Logging enabled.")
+else:
+    logging.getLogger().setLevel(logging.CRITICAL)
+
 
 #
 # Main App
