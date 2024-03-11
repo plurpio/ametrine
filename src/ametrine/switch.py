c36d894f14d78c75ebfc5f1fe41321a803addca4
Plurpio <148961679+plurpio@users.noreply.github.com>
2024-03-11 18:48:04 +1100

    switch: added symlinking of a theme's files

--- src/ametrine/switch.py
+++ src/ametrine/switch.py
@@ -18,6 +18,30 @@ def changeTheme(theme):
             logging.info("Running precmd: "+i)
             os.system(i)
 
+    for (root,dirs,files) in os.walk(themes.themePath(theme), topdown=True):
+        for i in files:
+            symlinkPath = os.path.join(root.replace(themes.themePath(theme), os.path.expanduser("~"))+"/"+i)
+            if symlinkPath == os.path.join(os.path.expanduser("~"), "config.yaml"):
+                logging.info("skipping config.yaml with path: "+root+"/"+i)
+                continue
+            if os.path.exists(os.path.dirname(symlinkPath)) == False:
+                os.makedirs(os.path.dirname(symlinkPath))
+                logging.info("creating dir: "+os.path.dirname(symlinkPath))
+            try:
+                if os.path.islink(symlinkPath) == True:
+                    os.remove(symlinkPath)
+                    logging.warning("overwriting symlink for path "+symlinkPath)
+                logging.info("symlinking "+os.path.join(root, i)+" to "+symlinkPath)
+                os.symlink(os.path.join(root, i), symlinkPath)
+            except:
+                if settings.setting("overwritefiles"):
+                    os.remove(symlinkPath)
+                    os.symlink(os.path.join(root, i), symlinkPath)
+                    logging.warning("overwrited "+symlinkPath+" with symlink")
+                else:
+                    logging.error("real file at location "+symlinkPath+" enable overwritefiles in config.yaml or delete file in that location.")
+
+
     if themes.themeConfig(theme, "aftercmds"):
         for i in themes.themeConfig(theme, "aftercmds"):
             logging.info("Running aftercmd for theme "+theme+": "+i)
