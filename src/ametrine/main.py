fd6278c7da3f77fd7889a15e1ecdb83af1a51501
Plurpio <148961679+plurpio@users.noreply.github.com>
2024-03-25 16:41:53 +1100

    cli: basic CLI working

--- src/ametrine/main.py
+++ src/ametrine/main.py
@@ -2,10 +2,12 @@
 # Imports
 #
 
-import sys
+import switch
 import logging
-import os
+import themes
 import click
+import os
+import data
 
 #
 # Configure pretty logging
@@ -17,7 +19,7 @@ logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%H:
 logging.basicConfig(format='%(asctime)s %(levelname)s %(module)s -> %(funcName)s: %(message)s', datefmt='%H:%M:%S', level=logging.ERROR)
 logging.basicConfig(format='%(asctime)s %(levelname)s %(module)s -> %(funcName)s: %(message)s', datefmt='%H:%M:%S', level=logging.CRITICAL)
 
-if os.getenv("DEBUG") or len(sys.argv) >= 2 and sys.argv[1] == "debug":
+if os.getenv("DEBUG"):
     logging.getLogger().setLevel(logging.DEBUG)
     logging.debug("Logging enabled.")
 else:
@@ -28,10 +30,43 @@ else:
 # Main App
 #
 
-if __name__ == "__main__":
-    print("bob")
-else:
-    import data
-    import switch
-    import themes
-    import settings
+@click.version_option("0a", prog_name="ametrine")
+@click.group()
+def cli():
+    pass
+
+@cli.group()
+def theme():
+    """commands related to themes"""
+    pass
+
+@theme.command()
+def ls():
+    """list all valid themes"""
+    for i in themes.getThemes():
+        print(i)
+
+@theme.command("config")
+def config():
+    """prints out default theme configuration"""
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "defaultFiles", "themeconfig.yaml")) as f: print(f.read())
+
+@cli.command()
+@click.argument("theme")
+def change(theme):
+    """change to a specific theme"""
+    switch.changeTheme(theme)
+
+@cli.command()
+@click.argument("theme", required=False)
+def cleanup(theme):
+    """remove all symlinks of a specific theme, use all for general cleanup"""
+    if theme in themes.getThemes(): switch.removeTheme(theme)
+    elif data.get("lastTheme") != "": switch.removeTheme(data.get("lastTheme"))
+    elif theme == "all": 
+        for i in themes.getThemes(): switch.removeTheme(i)
+    else: click.UsageError
+
+
+if __name__ == '__main__':
+    cli()
